bb56c54b039f19954d7e82573f2195efdc7a598c
Wendell Silva Soares <wensiso2000@gmail.com>
2017-11-09 17:18:08 -0300

    Finalizando projeto básico

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-package br.com.fat.jrsa;
-
 import java.io.BufferedWriter;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -15,16 +13,26 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 import org.apache.commons.cli.*;
+import org.apache.commons.cli.ParseException;
 
+import br.com.fat.jrsa.RSAKey;
+import br.com.fat.jrsa.RSA;
 
+/**
+ * 
+ * To encrypt:
+ * java Main -e data/sample/message.txt -o data/sample/encrypt_msg.dat  -k
+ * 
+ * To decrypt:
+ * java Main -d data/sample/encrypt_msg.dat -o data/sample/decod_message.txt
+ * 
+ * @author wendell
+ *
+ */
 public class Main {
 
 	static String pubk_filename = "./data/keys/pubk.txt";
 	static String privk_filename = "./data/keys/privk.txt";
-
-	static String msg_filename = "./data/sample/message.txt";
-	static String encrypt_filename = "./data/sample/encrypt_msg.dat";
-	static String msgdec_filename = "./data/sample/decod_message.txt";
 			
 	/**
 	 * Main
@@ -39,11 +47,41 @@ public class Main {
 		
 		System.out.println(prog);
 		
+		String msg_filename = "./data/sample/message.txt";
+		String encrypt_filename = "./data/sample/encrypt_msg.dat";
+		String out_filename = "./data/sample/decod_message.txt";
+		
+		boolean encrypt = false;
+		boolean decrypt = false;
+		
 		CommandLineParser parser = new DefaultParser();	
 		Options options = new Options();
-		options.addOption("e", false, "encriptar arquivo");
-		options.addOption("d", false, "desencriptar arquivo");
-		options.addOption("o", false, "arquivo de saída");
+		
+		Option enc = Option.builder("e")
+			    .longOpt( "encrypt" )
+			    .desc( "arquivo para encriptar"  )
+			    .hasArg()
+			    .argName( "msg_filename" )
+			    .build();
+		
+		Option desenc = Option.builder("d")
+			    .longOpt( "desencrypt" )
+			    .desc( "arquivo para desencriptar"  )
+			    .hasArg()
+			    .argName( "encrypt_filename" )
+			    .build();
+		
+		Option output = Option.builder("o")
+			    .longOpt( "output" )
+			    .desc( "arquivo de saída"  )
+			    .hasArg()
+			    .argName( "dest_filename" )
+			    .build();
+		
+		options.addOption(enc);
+		options.addOption(desenc);
+		options.addOption(output);
+		
 		options.addOption("k", false, "gera um novo par de chaves");
 		options.addOption("v", false, "imprime arquivo aberto");
 		
@@ -53,50 +91,74 @@ public class Main {
 			Scanner in = new Scanner(System.in);
 			String useropt = "n";
 			CommandLine cmd = parser.parse(options, args);
+			
+			if(cmd.hasOption("e")){
+				msg_filename = cmd.getOptionValue("e");
+				System.out.println("Arquivo para encriptar: " + msg_filename);
+				encrypt = true;
+			} else if(cmd.hasOption("d")) {
+				encrypt_filename = cmd.getOptionValue("d");
+				System.out.println("Arquivo para encriptar: " + encrypt_filename);
+				decrypt = true;
+			} else {
+				throw new ParseException("Arquivo de origem não informado");
+			}
+			
+			if(cmd.hasOption("o")) {
+				out_filename = cmd.getOptionValue("o");
+				System.out.println("Arquivo de destino: " + out_filename + "\n");
+			} else {
+				throw new ParseException("Arquivo de destino não informado");
+			}
+			
 			if(cmd.hasOption("k")) {
+				if(decrypt)
+					throw new ParseException("Não gerar nova chave para desencriptar...");
 			    System.out.println("Gerando as chaves");
 			    key.autoBuildKeys();
 			    key.savePublicKey(pubk_filename);
 				key.savePrivateKey(privk_filename);
 				System.out.println(key);
 				
-				System.out.println("Utilizar a chave recem-gerada? (S/N) ");
+				System.out.println("Reutilizar a chave recem-gerada? (S/N) ");
 				useropt = in.nextLine();
 			} 
 			if(useropt.startsWith("n") || useropt.startsWith("N")) {
-				//ask for keys
-				System.out.println("Informe a chave pública (n, e): ");
-				Long n = in.nextLong();
-				Long e = in.nextLong();
-				System.out.println("Sua chave pública: (" + n + ", " + e + ")\n");
-				System.out.println("Informe a chave privada (d): ");
-				Long d = in.nextLong();
-				System.out.println("Sua chave privada: (" + n + ", " + d + ")\n");
-				
-				key.setPubKey(n, e);
-				key.setPrivKey(n, d);
+				if (encrypt) {
+					// ask for keys
+					System.out.println("Informe a chave pública (n, e): ");
+					Long n = in.nextLong();
+					Long e = in.nextLong();
+					key.setPubKey(n, e);
+					System.out.println("Sua chave pública: (" + n + ", " + e + ")\n");
+				} else if (decrypt) {
+					System.out.println("Informe a chave privada (n, d): ");
+					Long n = in.nextLong();
+					Long d = in.nextLong();
+					key.setPrivKey(n, d);
+					System.out.println("Sua chave privada: (" + n + ", " + d + ")\n");
+				}
 			}
 			
 			in.close();
 			
 			RSA encoder = new RSA(key);
 			
-			//encoder.testAll("create view cotistas as\n"
-			//		+ "select * from alunos where cota=\"True\"");
-			
 			long begin = System.currentTimeMillis();
-			encrypt(msg_filename, encrypt_filename, encoder);
-			decode(encrypt_filename, msgdec_filename, encoder);
+			if(encrypt)
+				encrypt(msg_filename, out_filename, encoder);
+			else if (decrypt)
+				decode(encrypt_filename, out_filename, encoder);
 			long end =  System.currentTimeMillis();
 			
 			System.out.println("\nTempo decorrido: " + (end - begin) + " ms");
 			
 		} catch (ParseException e) {
+			System.out.println(e.getMessage());
 			System.out.println(usage + "\n");
 			e.printStackTrace();
 		}
 	}
-	
 	
 	/**
 	 * Encrypts an text file
